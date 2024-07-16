79831d5151aaed12c458565bd78e4b9af1b02ecc
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2024-07-16 09:21:21 +0300

    Update main.py

--- fastapi-postgres-docker/main.py
+++ fastapi-postgres-docker/main.py
@@ -12,9 +12,52 @@ app = _fastapi.FastAPI()
 
 
 @app.post("/api/accounts/", response_model=_schemas.Accounts)
-async def create_account(
-        accounts: _schemas.CreateAccount,
+async def create_accounts(
+        accounts: _schemas.CreateAccounts,
         db: _orm.Session = _fastapi.Depends(_services.get_db),
 ):
-    return await _services.create_account(accounts=accounts, db=db)
+    return await _services.create_accounts(accounts=accounts, db=db)
 
+
+@app.get("/api/accounts/", response_model=List[_schemas.Accounts])
+async def get_accounts(db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    return await _services.get_all_accounts(db=db)
+
+
+@app.get("/api/accounts/{account_id}/", response_model=_schemas.Accounts)
+async def get_account(
+    account_id: int, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    accounts = await _services.get_accounts(db=db, account_id=account_id)
+    if accounts is None:
+        raise _fastapi.HTTPException(status_code=404, detail="Account does not exist")
+
+    return accounts
+
+
+@app.delete("/api/accounts/{account_id}/")
+async def delete_account(
+    account_id: int, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    accounts = await _services.get_accounts(db=db, account_id=account_id)
+    if accounts is None:
+        raise _fastapi.HTTPException(status_code=404, detail="Account does not exist")
+
+    await _services.delete_accounts(accounts, db=db)
+
+    return "successfully deleted the user"
+
+
+@app.put("/api/accounts/{account_id}/", response_model=_schemas.Accounts)
+async def update_account(
+    account_id: int,
+    accounts_data: _schemas.CreateAccounts,
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    accounts = await _services.get_accounts(db=db, account_id=account_id)
+    if accounts is None:
+        raise _fastapi.HTTPException(status_code=404, detail="Account does not exist")
+
+    return await _services.update_accounts(
+        accounts_data=accounts_data, accounts=accounts, db=db
+    )
